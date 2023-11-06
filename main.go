cde18cf50e414c9764c7f97e326dd044e70bd659
bebeljoan <147100779+bebeljoan@users.noreply.github.com>
2023-11-06 03:35:58 +0100

    Update main.go

--- main.go
+++ main.go
@@ -1,22 +1,9 @@
 package main
 
-import (
-	"log"
-	"os"
-
-	"github.com/gin-gonic/gin"
-)
-
 func main() {
-	r := gin.Default()
-	r.GET("/", func(c *gin.Context) {
-		c.Status(204)
-	})
+	mux := http.NewServeMux()
 
-	httpPort := os.Getenv("HTTP_PORT")
-	if httpPort == "" {
-		httpPort = "8080"
-	}
+	mux.Handle("/", http.RedirectHandler("https://freshman.tech", http.StatusSeeOther))
 
-	log.Fatal(r.Run(":" + httpPort))
+	http.ListenAndServe(":6000", mux)
 }
